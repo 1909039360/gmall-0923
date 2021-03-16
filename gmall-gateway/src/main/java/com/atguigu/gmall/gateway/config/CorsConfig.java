80f35fb5a412c8316bd0b7b5aada1bf302763784
zhou0423 <1245067373@qq.com>
2021-03-16 20:28:42 +0800

    Initial commit

---
+++ gmall-gateway/src/main/java/com/atguigu/gmall/gateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.atguigu.gmall.gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        // 初始化cors配置类
+        CorsConfiguration config = new CorsConfiguration();
+        // 允许跨域访问的域名。*：代表所有域名可以跨域访问，但是不能携带cookie。要携带cookie，这里必须配置具体的域名
+        config.addAllowedOrigin("http://manager.gmall.com");
+        // 允许所有请求方式跨域访问
+        config.addAllowedMethod("*");
+        // 允许携带cookie
+        config.setAllowCredentials(true);
+        // 允许携带所有头信息
+        config.addAllowedHeader("*");
+
+        UrlBasedCorsConfigurationSource configurationSource = new UrlBasedCorsConfigurationSource();
+        configurationSource.registerCorsConfiguration("/**", config);
+        return new CorsWebFilter(configurationSource);
+    }
+}
---
+++ gmall-sms/src/main/java/com/atguigu/gmall/sms/service/impl/SkuBoundsServiceImpl.java
@@ -0,0 +1,72 @@
+package com.atguigu.gmall.sms.service.impl;
+
+import com.atguigu.gmall.sms.entity.SkuFullReductionEntity;
+import com.atguigu.gmall.sms.entity.SkuLadderEntity;
+import com.atguigu.gmall.sms.mapper.SkuFullReductionMapper;
+import com.atguigu.gmall.sms.mapper.SkuLadderMapper;
+import com.atguigu.gmall.sms.vo.SkuSaleVo;
+import org.bouncycastle.jcajce.provider.symmetric.AES;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.atguigu.gmall.common.bean.PageResultVo;
+import com.atguigu.gmall.common.bean.PageParamVo;
+
+import com.atguigu.gmall.sms.mapper.SkuBoundsMapper;
+import com.atguigu.gmall.sms.entity.SkuBoundsEntity;
+import com.atguigu.gmall.sms.service.SkuBoundsService;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+
+
+@Service("skuBoundsService")
+public class SkuBoundsServiceImpl extends ServiceImpl<SkuBoundsMapper, SkuBoundsEntity> implements SkuBoundsService {
+
+    @Autowired
+    private SkuLadderMapper ladderMapper;
+    @Autowired
+    private SkuFullReductionMapper reductionMapper;
+
+    @Override
+    public PageResultVo queryPage(PageParamVo paramVo) {
+        IPage<SkuBoundsEntity> page = this.page(
+                paramVo.getPage(),
+                new QueryWrapper<SkuBoundsEntity>()
+        );
+
+        return new PageResultVo(page);
+    }
+
+    @Transactional
+    @Override
+    public void saveSales(SkuSaleVo saleVo) {
+        //  3.1. 保存sms_sku_bounds
+        SkuBoundsEntity skuBoundsEntity = new SkuBoundsEntity();
+        BeanUtils.copyProperties(saleVo, skuBoundsEntity);
+        List<Integer> work = saleVo.getWork();
+        if (!CollectionUtils.isEmpty(work)){
+            skuBoundsEntity.setWork(work.get(3) * 8 + work.get(2) * 4 + work.get(1) * 2 + work.get(0));
+        }
+        this.save(skuBoundsEntity);
+
+        //  3.2. 保存sms_sku_full_reduction
+        SkuFullReductionEntity reductionEntity = new SkuFullReductionEntity();
+        BeanUtils.copyProperties(saleVo, reductionEntity);
+        reductionEntity.setAddOther(saleVo.getFullAddOther());
+        this.reductionMapper.insert(reductionEntity);
+
+        //  3.3. 保存sms_sku_ladder
+        SkuLadderEntity ladderEntity = new SkuLadderEntity();
+        BeanUtils.copyProperties(saleVo, ladderEntity);
+        ladderEntity.setAddOther(saleVo.getLadderAddOther());
+        this.ladderMapper.insert(ladderEntity);
+
+    }
+
+}
